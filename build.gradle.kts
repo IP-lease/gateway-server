e003028ad86d5d357cd2a858e4517fb12268e9e0
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-23 14:09:31 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.iplease"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-webflux")
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+    implementation ("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation ("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation ("org.springdoc:springdoc-openapi-webflux-ui:1.5.10")
+}
+
+
+dependencyManagement {
+    imports {
+        val springCloudVersion = "2021.0.0"
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
